1e31c3b9af914c74e250083e52fbb4bfdaf01991
Wendel <senshi13wendel@gmail.com>
2024-10-07 19:01:30 -0300

    sonfigurando uglify, e retirando o lighten do css main

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
 
 //compilação do sass
 function styles() {
@@ -15,7 +22,8 @@ function images() {
         .pipe(gulp.dest('./dist/images')); 
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)) //funcao criada para observar mudancas feitas nos arquivos scss assim executando a compilaçao do sass paralelamente.
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts)) 
 }
